194fde2973b587aea6e4cf876b9a0f2837a1f9c0
Viet Quan <nguyenvietquan.envietquan30032004@an30032004gmail.32004gmailcom>
2023-12-26 10:25:06 +0700

    upload lan 3

--- src/app/Controller/MiddlewareController.js
+++ src/app/Controller/MiddlewareController.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 export const verifyLogin = (req, res, next) => {
     const accessToken = req.headers?.token?.split(' ')[1];
     if (accessToken) {
-        jwt.verify(accessToken, 'mk', (err, user) => {
+        jwt.verify(accessToken, process.env.CODE_SIGN_JWT, (err, user) => {
             if (err) {
                 return res.status(403).json('Token is not valid');
             }
