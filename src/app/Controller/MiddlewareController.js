561cd3a551a10b734e3aa935aeb13249396a8a2f
Nguyenvietquan2004 <nguyenvietquan.30032004@gmail.com>
2024-07-14 09:16:58 +0700

    fsd

--- src/app/Controller/MiddlewareController.js
+++ src/app/Controller/MiddlewareController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 export const verifyLogin = (req, res, next) => {
     const accessToken = req.cookies.accessToken;
+    console.log(req.cookies);
     if (accessToken) {
         jwt.verify(accessToken, process.env.CODE_SIGN_JWT, (err, user) => {
             if (err) {
